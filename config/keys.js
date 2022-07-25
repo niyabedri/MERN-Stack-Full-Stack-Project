9142478a37f09d9e6adf7c90eef833f600e1a1a2
niya_bedri <niya.bedri44@gmail.com>
2022-07-25 23:51:15 +0300

    :hatching_chick: release : initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:admin@cluster0.qfy65.mongodb.net/react-social-dev-connector",
+    secretOrKey:"secret"
+};
---
+++ routes/api/users.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
+//Load User model
+const User = require("../../models/User");
+
+// @router  GET api/users/test
+// @desc    Tests users route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+
+// @router  GET api/users/register
+// @desc    Register users route
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({
+    email: req.body.email,
+  }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password,
+      });
+
+      bycrypt.genSalt(10, (err, salt) => {
+        bycrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            throw err;
+          }
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
+});
+// @router  GET api/users/register
+// @desc    Register users route
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then((user) => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+    bycrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        const payload = { id: user.id, name: user.name };
+
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer" + token,
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
+
+module.exports = router;
