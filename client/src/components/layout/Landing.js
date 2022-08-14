ddcf47340f24e1a91e7ec63870516536c375c5a2
niya_bedri <niya.bedri44@gmail.com>
2022-08-14 22:18:06 +0300

    :zap: changes on landing page(store and props)

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
 class Landing extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      window.location.href = "/dashboard";
+    }
+  }
   render() {
     return (
       <div className="landing">
@@ -28,5 +35,11 @@ class Landing extends Component {
     );
   }
 }
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
 
-export default Landing;
+export default connect(mapStateToProps)(Landing);
